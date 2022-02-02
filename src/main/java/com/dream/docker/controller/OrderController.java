7939128d5cb938536d88966badce84f880a45c68
huzejun <huzejun1990>
2022-02-02 20:31:28 +0800

    初次上传-需要再微调+2022年2月2日20:30:32

---
+++ src/main/java/com/dream/docker/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.dream.docker.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Author : huzejun
+ * @Date: 2022/1/30-0:23
+ */
+@RestController
+public class OrderController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/order/docker")
+    public String  helloDocker(){
+
+        return "hello docker" + "\t" + port + "\t" + UUID.randomUUID().toString();
+    }
+
+    @RequestMapping(value = "/order/index",method = RequestMethod.GET)
+    public String index(){
+        return "服务端口号： " +  "\t" + UUID.randomUUID().toString();
+    }
+}
